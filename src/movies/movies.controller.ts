b766ec53a43c3ea8bbb4fada84cbdf8980541931
Ola Ishola <olaishola@hotmail.co.uk>
2023-02-12 18:18:53 +0100

    removed api slash route

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 
-@Controller('api/movies')
+@Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) { }
 
